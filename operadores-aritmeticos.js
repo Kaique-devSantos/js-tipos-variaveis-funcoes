f2d4e594c18a550f8df1b2f0580498e069748d18
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-03-06 14:54:56 -0300

    incluindo e entendendo novos conceitos

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,8 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = 7;
+const notaQuartoBi = 9.3 ;
+
+
+
+const media = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi)/4;
